e42f73ff5758cad372c59f8b6531db8a98ba0745
sanu.sn <sanu.sn@PIA3270>
2022-12-09 11:32:33 +0530

    Add project files.

---
+++ LamdaFunc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LamdaExpression
+{
+    class LamdaFunc
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addNum = (int firstnum, int secondnum) => firstnum + secondnum;
+
+            Func<int, int, int> subNum = (int firstnum, int secondnum) => firstnum - secondnum;
+
+            Func<int, int, int> mulNum = (int firstnum, int secondnum) => firstnum * secondnum;
+
+            Func<int, int, int> divNum = (int firstnum, int secondnum) => firstnum / secondnum;
+
+            Calculator(addNum, 50, 10);
+            Calculator(subNum, 50, 10);
+            Calculator(mulNum, 50, 10);
+            Calculator(divNum, 50, 10);        
+        }
+
+        static void Calculator(Func<int, int, int> cal, int firstnum, int secondnum)
+        {
+            Console.WriteLine(cal(firstnum,secondnum));
+        }
+    }
+    
+}
