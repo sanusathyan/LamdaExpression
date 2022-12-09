b38532c605abc8090f69564471e853af4a1f2e8d
sanusathyan <119839627+sanusathyan@users.noreply.github.com>
2022-12-09 11:34:05 +0530

    Add files via upload

---
+++ LambaAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LambaAction
+{
+    class LambaAction
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> addNum = (int firstnum, int secondnum) =>
+            Console.WriteLine(firstnum + secondnum);
+
+            Action<int, int> subNum = (int firstnum, int secondnum) =>
+            Console.WriteLine(firstnum - secondnum);
+
+            Action<int, int> mulNum = (int firstnum, int secondnum) =>
+            Console.WriteLine(firstnum * secondnum);
+
+            Action<int, int> divNum = (int firstnum, int secondnum) =>
+            Console.WriteLine(firstnum / secondnum);
+
+            calculator(addNum += subNum += mulNum += divNum, 50, 10);
+        }
+
+        static void calculator(Action<int,int> cal,int firstnum, int secondnum)
+        {
+            cal(firstnum, secondnum);
+        }
+    }
+}
